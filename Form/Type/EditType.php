bba72b0cabc64506e67328a4d81701f05e1c2068
Fabio Dellutri <fabio.dellutri@mitecube.com>
2014-01-09 15:16:40 +0100

    last modifications

--- Form/Type/EditType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Manticora\CMSBundle\Form\Type\Route;
-
-use Admingenerated\ManticoraCMSBundle\Form\BaseRouteType\EditType as BaseEditType;
-
-class EditType extends BaseEditType
-{
-}
